e0e7d9e8b04f03d436a8330beed8bd4453e67e2d
THE ONE <THE ONE@DESKTOP-AO9U392>
2022-07-27 11:52:19 +0530

    First commit
    Uploading all files

---
+++ demo/src/demo/Employee.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Employee 
+{
+	public int getName(int empNo)
+	{
+		int emp = empNo;
+		return emp;
+	}
+	
+	public String getName(String empName)
+	{
+		String emp = empName;
+		return emp;
+	}
+}
